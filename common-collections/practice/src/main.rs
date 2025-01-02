c3d389dbe93c39dcc6dde8737b1dade11c650908
Pranav Vinodan <pranavvinodan@Pranavs-MacBook-Pro.local>
2025-01-03 00:44:42 +0530

    Commit

--- common-collections/practice/src/main.rs
+++ common-collections/practice/src/main.rs
@@ -1,4 +1,5 @@
 // Create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
+// Implement the following:
 use std::collections::HashMap;
 use std::io;
 
